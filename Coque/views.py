c34866aa99a956ff4a8fa68b2e9a4b4c377fceb9
CrossGrain29506 <saulomar121@outlook.com>
2023-02-15 21:08:52 -0700

    commit 1

---
+++ Coque/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
+
+# Create your views here. 
+def hello(request):
+ return render(request,'./index.html')
+
+def login(request):
+  return render(request,'./login.html')
+  
+def reg(request):
+  return render(request,'./reg.html')
